16b849b4392cf4888295b3c571df470dac238692
DavidArranz <77276639+DavidArranz@users.noreply.github.com>
2021-10-11 09:58:31 +0200

    Add files via upload

---
+++ T5ep10.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package t5ep10;
+
+/**
+ *
+ * @author Carlos Povill
+ */
+public class T5ep10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i=1;
+        while(i<100){
+            if(i%5!=0){
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+    
+}
