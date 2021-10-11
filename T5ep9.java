42bfd4864f4eef845df797efcfd8ed416b828a8e
DavidArranz <77276639+DavidArranz@users.noreply.github.com>
2021-10-11 09:45:51 +0200

    Add files via upload

---
+++ T5ep9.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package t5ep9;
+
+/**
+ *
+ * @author Carlos Povill
+ */
+public class T5ep9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int i=0;
+       while (i<100){
+           
+           System.out.println(i);
+        i=i+2;
+        }
+    }
+    
+}
